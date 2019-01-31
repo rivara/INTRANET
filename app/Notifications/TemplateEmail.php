7846ef7310e3c096242c4717c48d816876ba3d6d
rvalle <rvalle@comafe.es>
2019-01-31 16:54:57 +0100

    Control de Mail (reseteo de passwords)

---
+++ app/Notifications/TemplateEmail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class TemplateEmail extends Notification
+{
+    use Queueable;
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+
+        public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Ha recibido este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para su cuenta')
+            ->action('Reseteo Password', route('admin.password.reset', $this->token))
+            ->line('Si no solicitó un restablecimiento de contraseña, ignore este correo electrónico.');
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
