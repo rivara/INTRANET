a06586600c67d9fd290763325794db5df9c1e243
rvalle <rvalle@comafe.es>
2019-02-06 13:14:34 +0100

    Boton de ayuda (?)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,8 @@
     <meta http-equiv="cache-control" content="private, max-age=0, no-cache">
     <meta http-equiv="pragma" content="no-cache">
     <meta http-equiv="expires" content="0">
-
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Portal-Empresa') }}</title>
     <!-- Scripts -->
     <script src="{{ asset('js/jquery-3.3.3.min.js') }}"></script>
@@ -28,12 +25,10 @@
             $('.input3').iconpicker(".input3");
         });
     </script>
-
     @stack('scripts')
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
@@ -44,20 +39,14 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-               <!-- <a class="navbar-brand" href="{# url('/') #}">
-                    {# config('app.name', 'Laravel')#}
-                </a>-->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto"></ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
-
                         @if (isset($nombre[0]))
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" >
                                 @csrf
@@ -66,14 +55,30 @@
                                 </button>
                             </form>
                         @endif
+                            <button type="submit"   class=" btn btn-circle btn-outline-info fa fa-question"
+                                    data-toggle="modal"
+                                    data-target="#confirm" value="aa">
+                            </button>
                     </ul>
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
     </div>
+    <!-- Modal Dialog -->
+    <div class="modal fade" id="confirm" aria-labelledby="confirmDeleteLabel">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <h6>si tienes problemas  contacte con informatica</h6>
+                        <a href="mailto:informatica@comafe.es">
+                            informatica@comafe.es
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
 </body>
 </html>
