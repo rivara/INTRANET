9e8b68f205ce14020f010596b7aa2733f10b99c2
rvalle <rvalle@comafe.es>
2019-04-17 16:37:42 +0200

    reporting

--- resources/views/reporting/index.blade.php
+++ resources/views/reporting/index.blade.php
@@ -1,6 +1,5 @@
 
 @include('reporting.bar')
-
             @if(isset($option))
                 @if($option =="IndiceDeRotacion")
                     <form action="{{ route('indiceDeRotacion')}}" method="get">
@@ -13,7 +12,6 @@
                 @elseif ($option=="otros")
                     <h3>en construccion</h3>
                     <i class="fa fa-user-circle-o" aria-hidden="true"></i>
-
                 @endif
             @endif
 
