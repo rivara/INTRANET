5b21c66b124ca45583a3744c156ac174028a125d
rvalle <rvalle@comafe.es>
2019-04-30 14:08:54 +0200

    reporting version beta

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -133,14 +133,14 @@ from articulos a left outer join familias f on a.familia_id = f.id
                     'articulos.tipo_producto',
                     'articulos.tipo_rotacion',
                     'articulos.proveedor_id',
-                   // 'proveedores.nombre as razon_social',
+                    'proveedores.nombre as razon_social',
                     'articulos.referencia_proveedor',
-                  //  'proveedores.comprador_id',
+                    'proveedores.comprador_id',
                     'articulos.marca',
                     'articulos.es_merch_ferrokey',
                     'articulos.coste_medio',
                     'familias.id as familiaId',
-                     'familias.nombre as familiaNombre'
+                    'familias.nombre as familiaNombre'
                    /* $db->raw("(select substring(id,1,2) as f from familias where id=".'familiaId'.")  as fam1") ,
                     $db->raw("(select familias.nombre from familias where id=fam1 ) as desc1"),
                     $db->raw("(select substring(id,1,4) as f from familias where id=".'familiaId'.")  as fam2") ,
@@ -152,7 +152,7 @@ from articulos a left outer join familias f on a.familia_id = f.id
                     $db->raw("(select sum(precio) from historico_ventas_detalle where articulo_id=articulos.id ) as ventasPvp"),
                     $db->raw("(select sum(precio/coste_medio) from historico_ventas_detalle where articulo_id=articulos.id ) as ventasPMedio")*/
                 )
-               // ->join('proveedores', 'proveedores.id', '=', 'articulos.proveedor_id')
+                ->join('proveedores', 'proveedores.id', '=', 'articulos.proveedor_id')
                 ->join('familias', 'familias.id', '=', 'articulos.familia_id')
                // ->join('articulos_almacen', 'articulos_almacen.articulo_id', '=', 'articulos.id')
              ->where($where[0][0], $where[0][1], $where[0][2])
