527fb25fc184889cd539e2ebccdebcc5799d89ae
rvalle <rvalle@comafe.es>
2019-05-03 12:18:13 +0200

    reporting version beta

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -148,16 +148,16 @@ class reportingController
                 $db->raw("(select substring(id,1,6) as f from familias where id=" . 'familiaId' . ")  as fam3"),
                 $db->raw("(select familias.nombre from familias where id=fam3 ) as desc3"),
                 'articulos_almacen.es_extinguir as es_extinguir',
-                //$db->raw("(select count(articulo_id) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as ventasUds")
-                $db->raw("(select sum(articulo_id) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as ventasPvp")
-                //$db->raw("(select sum(precio*coste_medio) from historico_ventas_detalle where articulo_id =articulos.id) as ventasPMedio")
-                //'articulos_almacen.stock_actual as stock_actual',
-                //$db->raw("(select sum(articulo_id) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as MargenBruto"),
+                $db->raw("(select count(articulo_id) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as ventasUds"),
+                $db->raw("(select sum(articulo_id) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as ventasPvp"),
+                $db->raw("(select sum(precio*coste_medio) from historico_ventas_detalle where articulo_id =articulos.id) as ventasPMedio"),
+                'articulos_almacen.stock_actual as stock_actual',
+                $db->raw("(select sum(articulo_id) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as MargenBruto"),
                 //MARGEN BRUTO de momento sin margen
-                //$db->raw("(select avg(mad_stock) from stock_medio where articulo_id =articulos.id) as stockMedio"),
-                //$db->raw("(select (count(articulo_id)/stock_actual) from historico_ventas_detalle where idArticulos =articulos.id group by articulo_id) as indice"),
-                //$db->raw("(select sum(articulo_id)/indice from historico_ventas_detalle where idArticulos =articulos.id group by articulo_id) as MargenPorRotacion"),
-                //'articulos_almacen.es_surtido_alicante as surtido'
+                $db->raw("(select avg(mad_stock) from stock_medio where articulo_id =articulos.id) as stockMedio"),
+                $db->raw("(select (count(articulo_id)/stock_actual) from historico_ventas_detalle where idArticulos =articulos.id group by articulo_id) as indice"),
+                $db->raw("(select sum(articulo_id)/indice from historico_ventas_detalle where idArticulos =articulos.id group by articulo_id) as MargenPorRotacion"),
+                'articulos_almacen.es_surtido_alicante as surtido'
             )
             ->join('proveedores', 'proveedores.id', '=', 'articulos.proveedor_id')
             ->join('familias', 'familias.id', '=', 'articulos.familia_id')
