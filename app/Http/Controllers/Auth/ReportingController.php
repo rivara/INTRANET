06fda0459efac596d7a2960960a6c69dea2ef307
rvalle <rvalle@comafe.es>
2019-05-03 12:43:12 +0200

    reporting version beta

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -156,7 +156,7 @@ class reportingController
                 //MARGEN BRUTO de momento sin margen
                 $db->raw("(select avg(mad_stock) from stock_medio where articulo_id =articulos.id) as stockMedio"),
                 $db->raw("(select (count(articulo_id)/stock_actual) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as indice"),
-                $db->raw("(select (sum(articulo_id)/indice) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as MargenPorRotacion"),
+                //$db->raw("(select (sum(articulo_id)/indice) from historico_ventas_detalle where articulo_id =articulos.id group by articulo_id) as MargenPorRotacion"),
                 'articulos_almacen.es_surtido_alicante as surtido'
             )
             ->join('proveedores', 'proveedores.id', '=', 'articulos.proveedor_id')
