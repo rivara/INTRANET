7a0e0eb24dba66bc18c5e9569066d8bc037d0d41
rvalle <rvalle@comafe.es>
2019-05-03 12:08:56 +0200

    reporting version beta

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -147,7 +147,7 @@ class reportingController
                 $db->raw("(select familias.nombre from familias where id=fam2 ) as desc2"),
                 $db->raw("(select substring(id,1,6) as f from familias where id=" . 'familiaId' . ")  as fam3"),
                 $db->raw("(select familias.nombre from familias where id=fam3 ) as desc3"),
-                'articulos_almacen.es_extinguir as es_extinguir',
+                'articulos_almacen.es_extinguir as es_extinguir'
                /* $db->raw("(select count(articulo_id) from historico_ventas_detalle where idArticulos =articulos.id group by articulo_id) as ventasUds"),
                 $db->raw("(select sum(articulo_id) from historico_ventas_detalle where idArticulos =articulos.id group by articulo_id) as ventasPvp"),
                 $db->raw("(select sum(precio*coste_medio) from historico_ventas_detalle where idArticulos =articulos.id) as ventasPMedio"),
