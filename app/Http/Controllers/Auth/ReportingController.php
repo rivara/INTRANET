9d2b7784fbd767a42841d099fdc792c6c1406751
rvalle <rvalle@comafe.es>
2019-05-22 11:25:22 +0200

    reporting version beta -> prueba

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -36,9 +36,7 @@ class reportingController
         //VARIABLES
         $almacen = $request["almacen"];
         $fechaDesde = $request["fechaDesde"];
-        $fechaDesd=date('d-m-y',strtotime($fechaDesde));
         $fechaHasta = $request["fechaHasta"];
-        $fechaHast =date('d-m-y',strtotime($fechaHasta));
 
         $proveedor_id = $request["proveedor"];
         $familia_id = $request["familia"];
@@ -88,39 +86,19 @@ class reportingController
             "EXTINGUIR",
             "VENTAS (UDS)",
             "VENTAS (PVP)",
-            "VENTAS(PMEDIO)",
+            "VENTAS(PMEDIO)", //FALTA
             "STOCK ACTUAL",
-            "MARGEN BRUTO",
+            "MARGEN BRUTO",   // FALTA
             "STOCK MEDIO (UDS)",
-            "INDICE ROTACION",
+            "INDICE ROTACION", // FALTA
             "MARGEN POR ROTACION",
             "SURTIDO",
             "EJECUCUION"
         );
 
-        //consulta
-        $where = array();;
-        if (!empty($familia_id)) {
-            if ($niveles) {
-                $where[] = array('familia_id', 'like', $familia_id . '%');
-            } else {
-                $where[] = array('familia_id', '=', $familia_id);
-            }
-
-        } else {
-            $where[] = array('familia_id', 'like', '%');
-        }
 
 
-        if (!empty($proveedor_id)) {
-            $where[] = array_push($where, ['proveedor_id', '=', $proveedor_id]);
-        } else {
-            $where[] = array_push($where, ['proveedor_id', 'like', '%']);
-        }
 
-
-        // FALTA AÑADIR FECHA  RVR
-        //STOCK MEDIO (UDS)
         if($almacen=='PRINCIPAL'){
             $query="(select avg(ali_stock) from stock_medio  where articulo_id =articulos.id  GROUP BY articulo_id)as stockMedio";
 
@@ -131,60 +109,14 @@ class reportingController
 
 
 
-/*
-        $data = $db->table('articulos')
-            ->select(
-                'articulos.id as idArticulos',
-                'articulos.nombre',
-                'articulos.fecha_alta',
-                'articulos.fecha_baja',
-                'articulos.tipo_producto',
-                'articulos.tipo_rotacion',
-                'articulos.proveedor_id',
-                'proveedores.nombre as razon_social',
-                'articulos.referencia_proveedor',
-                'proveedores.comprador_id',
-                'articulos.marca',
-                'articulos.es_merch_ferrokey',
-                'articulos.coste_medio as costeMedio',
-                'familias.id as familiaId',
-                'familias.nombre as familiaNombre',
+        //consultas
 
-
-                $db->raw("( select substring(id,1,2) as f from familias where id=" . 'familiaId' . ")  as fam1"),
-                $db->raw("(select familias.nombre from familias where id=fam1 ) as desc1"),
-                $db->raw("(select substring(id,1,4) as f from familias where id=" . 'familiaId' . ")  as fam2"),
-                $db->raw("(select familias.nombre from familias where id=fam2 ) as desc2"),
-                $db->raw("(select substring(id,1,6) as f from familias where id=" . 'familiaId' . ")  as fam3"),
-                $db->raw("(select familias.nombre from familias where id=fam3 ) as desc3"),
-
-
-                'articulos_almacen.es_extinguir as es_extinguir',
-                $db->raw("now()"))
-                ->join('proveedores', 'proveedores.id', '=', 'articulos.proveedor_id')
-                ->join('familias', 'familias.id', '=', 'articulos.familia_id')
-                ->join('articulos_almacen', 'articulos_almacen.articulo_id', '=', 'articulos.id')
-                ->whereBetween('articulos.fecha_actualizacion', array($fechaDesde, $fechaHasta))
-                ->where($where[0][0], $where[0][1], $where[0][2])
-                ->where($where[1][0], $where[1][1], $where[1][2])
-                ->where("articulos_almacen.almacen","like",$almacen)
-                ->get();
-
-*/
-
-
-        //consulta
-        $where = array();;
         if (!empty($familia_id)) {
-            if ($niveles) {
-                $where[] = array('familia_id', 'like', $familia_id . '%');
+            $familia="and a.proveedor_id = '".$familia_id."'";
             } else {
-                $where[] = array('familia_id', '=', $familia_id);
+            $familia = "";
             }
 
-        } else {
-            $where[] = array('familia_id', 'like', '%');
-        }
 
 
         if (!empty($proveedor_id)) {
@@ -193,7 +125,13 @@ class reportingController
             $proveedor=" ";
         }
 
+        if (!empty($proveedor_id)) {
+            $proveedor="and a.proveedor_id = '".$proveedor_id."'";
+        } else {
+            $proveedor=" ";
+        }
 
+        $fecha="AND v.fecha  BETWEEN '".$fechaDesde."' AND '".$fechaHasta."'";
 
 
 
@@ -214,7 +152,7 @@ class reportingController
         pro.comprador_id,
         a.marca,
         a.es_merch_ferrokey,
-        a.coste_medio,
+        a.coste_medio ,
         a.familia_id,
         fam.ampliada, 
 		substring(a.familia_id,1,2) n1,
@@ -223,50 +161,35 @@ class reportingController
 		fam2.nombre,
 	    substring(a.familia_id,1,6) n6,
 		fam3.nombre,
-		ae.es_extinguir,
-		substring(fam.nombre,1,2) familias,
+		ae.es_extinguir as ext,
         ifnull(ven.CANSUM,0) as VENTA, 
-                
-                        alm.almacen FROM articulos a
-                                                LEFT OUTER JOIN (
-                                                    select articulo_id art,SUM(cantidad) CANSUM
-                                                    from historico_ventas_detalle v LEFT OUTER JOIN articulos a ON v.articulo_id = a.id
-                                                    WHERE empresa=1 
-                                                    AND year(fecha)=2018 
-                                                    AND es_directo=0 
-                                                    AND a.fecha_baja is null
-                                                    ".$proveedor."
-                                                    GROUP BY articulo_id
-                                                ) ven ON a.id = ven.art
-                                                LEFT OUTER JOIN articulos_almacen alm ON a.id = alm.articulo_id AND alm.almacen = 'PRINCIPAL'
-                                                LEFT JOIN proveedores pro ON a.proveedor_id = pro.id 
-                                                LEFT JOIN familias fam ON a.familia_id = fam.id 
-                                                LEFT JOIN familias fam1 ON substring(a.familia_id,1,2) = fam1.id 
-										        LEFT JOIN familias fam2 ON substring(a.familia_id,1,4) = fam2.id 
-										        LEFT JOIN familias fam3 ON substring(a.familia_id,1,6) = fam3.id 
-										        
-										        LEFT JOIN articulos_almacen ae ON a.id = ae.articulo_id 
-										        
-                                                WHERE a.fecha_baja is null
-                                                   ".$proveedor."
-                                                ORDER BY a.id)"));
-        //ar_dump($data);
-        //die;
-
-
-         //select articulo_id ID,SUM(cantidad) CANSUM  from historico_ventas_detalle  WHERE empresa=1 AND year(fecha)=2018 AND es_directo=0 GROUP BY articulo_id
-
-        // QUERY SANTI
-        /*$data = $db->select($db->raw("(select SUM(det.cantidad) as cantidad
-                 from historico_ventas_detalle det inner join historico_ventas cab ON det.empresa = cab.empresa AND det.tipo_documento = cab.tipo_documento AND det.documento = cab.documento
-                 WHERE cab.almacen ='".$almacen."' AND det.articulo_id = articulos.id group by articulo_id) as sumcantidad"));
-            /*
-            $db->raw("(select SUM(det.importe)
-                 from historico_ventas_detalle det inner join historico_ventas cab ON det.empresa = cab.empresa AND det.tipo_documento = cab.tipo_documento AND det.documento = cab.documento
-                 WHERE cab.almacen = '".$almacen."' AND det.articulo_id = articulos.id group by articulo_id) as sumimporte")
-            */
-
-
+        ifnull(ven.CANIMP,0) as IMPORTE,
+        a.coste_medio * ven.CANSUM as costeMedio,
+        ae.es_surtido_alicante as surtido
+                                FROM articulos a
+                                LEFT OUTER JOIN (
+                                    select articulo_id art,SUM(cantidad) CANSUM ,SUM(importe) CANIMP
+                                    from historico_ventas_detalle v LEFT OUTER JOIN articulos a ON v.articulo_id = a.id
+                                    WHERE empresa=1 
+                                    AND es_directo=0 
+                                    AND a.fecha_baja is null
+                                    ".$proveedor."
+                                    ".$familia."  
+                                    ".$fecha."  
+                                    GROUP BY articulo_id
+                                ) ven ON a.id = ven.art
+                                
+                                LEFT OUTER JOIN articulos_almacen alm ON a.id = alm.articulo_id AND alm.almacen = '".$almacen."'
+                                LEFT JOIN proveedores pro ON a.proveedor_id = pro.id 
+                                LEFT JOIN familias fam ON a.familia_id = fam.id 
+                                LEFT JOIN familias fam1 ON substring(a.familia_id,1,2) = fam1.id 
+                                LEFT JOIN familias fam2 ON substring(a.familia_id,1,4) = fam2.id 
+                                LEFT JOIN familias fam3 ON substring(a.familia_id,1,6) = fam3.id 
+                                LEFT JOIN articulos_almacen ae ON a.id = ae.articulo_id 
+                                
+        WHERE a.fecha_baja is null ".$proveedor." ".$familia."  ORDER BY a.id)"));
+      //  var_dump($data);
+       // die;
 
         $bg = array("808080", "0000ff", "B5BF00");
         // nombre de pestaña
