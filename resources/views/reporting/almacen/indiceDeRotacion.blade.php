8dce1dc3ccea7d7b87183448698fe5dc1f1c6613
rvalle <rvalle@comafe.es>
2019-05-24 16:19:19 +0200

    reporting version beta -> prueba

--- resources/views/reporting/almacen/indiceDeRotacion.blade.php
+++ resources/views/reporting/almacen/indiceDeRotacion.blade.php
@@ -1,4 +1,4 @@
-<?php ini_set('MAX_EXECUTION_TIME',0); ?>
+<?php ini_set('MAX_EXECUTION_TIME',1000); ?>
 @csrf
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <div class="row wrapperReporting center marginBottom20px">
