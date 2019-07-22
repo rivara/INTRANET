5ca4fc9a89c16cfcd9f8d16bb04616c3909bfc81
rvalle <rvalle@comafe.es>
2019-07-22 10:50:31 +0200

    Informe de marca propia

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -1019,7 +1019,7 @@ class reportingController
                 "F" => NumberFormat::FORMAT_NUMBER,
                 "G" => NumberFormat::FORMAT_CURRENCY_EUR,
                 "H" => NumberFormat::FORMAT_PERCENTAGE,
-                "I" => NumberFormat::FORMAT_PERCENTAGE_00,
+                "I" => NumberFormat::FORMAT_PERCENTAGE,
                 "J" => NumberFormat::FORMAT_NUMBER
 
             ];
