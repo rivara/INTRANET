2630c1f1613e656c4f666a170306d9dbd2903f19
rvalle <rvalle@comafe.es>
2019-08-14 11:31:04 +0200

    cambios

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -1222,7 +1222,7 @@ class reportingController
             $cliente= " ";
         }else{
             $proveedor= "";
-            $cliente= "AND c.cliente = '".$request['valor']."'";
+            $cliente= "AND c.cliente_id = '".$request['valor']."'";
         }
 
         if($tipoGrupoCliente=="TODOS"){
@@ -1251,7 +1251,13 @@ class reportingController
             ORDER BY c.cliente, p.id
           )"));
 
-
+        if($tipo=="PROVEEDOR"){
+            $proveedor= "AND p.id = '".$request['valor']."'";
+            $cliente= " ";
+        }else{
+            $proveedor= "";
+            $cliente= "AND c.cliente = '".$request['valor']."'";
+        }
 
 
 
