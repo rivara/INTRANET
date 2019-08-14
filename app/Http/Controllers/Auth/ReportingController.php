fcfa4be7e3b35c8308da26e42877382ad1ec9291
rvalle <rvalle@comafe.es>
2019-08-14 10:17:40 +0200

    cambios

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -1292,9 +1292,8 @@ class reportingController
             INNER JOIN historico_ventas_detalle det ON c.empresa = det.empresa AND c.tipo_documento = det.tipo_documento AND c.documento = det.documento
             LEFT OUTER JOIN articulos art ON det.articulo_id = art.id
             WHERE c.empresa = 1 
-            ".$cliente."
+            AND c.cliente_id = '139' 
             AND DATE_FORMAT(c.fecha, '%Y') = 2019
-            AND det.proveedor_id  like ".$lists[$i]->proveedor_id."
             GROUP BY det.proveedor_id
             ORDER BY det.fecha_actualizacion desc
             )"));
