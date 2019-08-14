95d34a87a59c28e323a1dab53b6ab5d8e301965d
rvalle <rvalle@comafe.es>
2019-08-14 10:51:02 +0200

    cambios

--- app/Http/Controllers/Auth/ReportingController.php
+++ app/Http/Controllers/Auth/ReportingController.php
@@ -1298,6 +1298,8 @@ class reportingController
             GROUP BY det.proveedor_id
             ORDER BY det.fecha_actualizacion desc
             )"));
+            var_dump($array2);
+            die();
             if (! isset($array2)) {
                 $data[$i][7] = $array2[0]->ventas;
             }
